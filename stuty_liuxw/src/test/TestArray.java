2bfbbea7bc96b72e1a262d2e270b4d1d560d7f39
liuxw <359675666@qq.com>
2017-03-27 16:28:12 +0800

    0327

--- stuty_liuxw/src/test/TestArray.java
+++ stuty_liuxw/src/test/TestArray.java
@@ -2,25 +2,23 @@ package test;
 
 public class TestArray {
    public static void main(String[] args) {
-      // 数组大小
-      int size = 20;
-      // 定义数组
-      double[] myList = new double[20];
-      myList[0] = 5.6;
-      myList[1] = 4.5;
-      myList[2] = 3.3;
-      myList[3] = 13.2;
-      myList[4] = 4.0;
-      myList[5] = 34.33;
-      myList[6] = 34.0;
-      myList[7] = 45.45;
-      myList[8] = 99.993;
-      myList[9] = 11123;
+      double[] myList = {1.9, 2.9, 3.4, 3.5};
+ 
+      // 打印所有数组元素
+      for (int i = 0; i < myList.length; i++) {
+         System.out.println(myList[i] + " ");
+      }
       // 计算所有元素的总和
       double total = 0;
-      for (int i = 0; i < size; i++) {
+      for (int i = 0; i < myList.length; i++) {
          total += myList[i];
       }
-      System.out.println("总和为： " + total);
+      System.out.println("Total is " + total);
+      // 查找最大元素
+      double max = myList[0];
+      for (int i = 1; i < myList.length; i++) {
+         if (myList[i] > max) max = myList[i];
+      }
+      System.out.println("Max is " + max);
    }
 }
