5de0e4f73f35f6f6d76098ec543dc61f8abbc967
liuxw <359675666@qq.com>
2017-03-27 13:27:47 +0800

    修改文件

--- stuty_liuxw/src/test/MainDir.java
+++ stuty_liuxw/src/test/MainDir.java
@@ -1,20 +1,20 @@
 package test;
 
 import java.io.File;
- 
+
 public class MainDir {
-    public static void main(String[] argv) throws Exception {
-//        File dir = new File("../test_caverage");
-        File dir = new File("src/test");
-        String[] children = dir.list();
-        if (children == null) {
-            System.out.println("该目录不存在");
-        }
-        else {
-            for (int i = 0; i < children.length; i++) {
-                String filename = children[i];
-                System.out.println(filename);
-            }
-        }
-    }
+	public static void main(String[] argv) throws Exception {
+		// File dir = new File("../test_caverage");
+		File dir = new File("src/test");
+		String[] children = dir.list();
+		if (children == null) {
+			System.out.println("该目录不存在");
+		} else {
+			// 如果目录存在，就打印目录里面的内容
+			for (int i = 0; i < children.length; i++) {
+				String filename = children[i];
+				System.out.println(filename);
+			}
+		}
+	}
 }
