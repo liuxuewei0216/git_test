8b074f71cc630e89722848bcc01e8caf0d8137f8
liuxw <359675666@qq.com>
2017-03-28 17:34:19 +0800

    新增文件

---
+++ Python/indent.py
@@ -0,0 +1,8 @@
+Number1 = int(input("enter the first number:"))
+Number2 = int(input("enter the second number:"))
+if Number1!=Number2:
+ print("Number1不等于Number2")
+elif Number1+Number2==10:
+ print("Number1和Number2的和为10")
+else:
+ print("The sum is",Number1+Number2)
